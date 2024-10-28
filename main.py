1a825246e39403057ade304919807cb8337f8446
mariasatelis <dudasatelis14@gmail.com>
2024-10-28 10:41:58 -0300

    [Atualizações]

--- main.py
+++ main.py
@@ -25,10 +25,10 @@ RED = (255,0,0)
 GREEN = (0,255,0) 
 BLUE = (0,0,255)
 
-background = pygame.image.load('./img/bgk.png')                                  #game background
-font = pygame.font.Font(os.path.join(os.getcwd(), './fonts/PDBI.ttf'), 42)
-score_text = font.render('Score : ' + str(score), True, (255, 255, 255))    #score display
-lives_icon = pygame.image.load('./img/w_h.png')                    #images that shows remaining lives
+background = pygame.image.load('./img/bgk.png')                                  # game background
+font = pygame.font.Font(os.path.join(os.getcwd(), './fonts/DMSerifText-Regular.ttf'), 42)  # Font PDBI for scores
+score_text = font.render('Score : ' + str(score), True, WHITE)                # score display
+lives_icon = pygame.image.load('./img/w_h.png')                               # images that show remaining lives
 
 # Generalized structure of the fruit Dictionary
 def generate_random_fruits(fruit):
@@ -49,8 +49,7 @@ def generate_random_fruits(fruit):
     else:
         data[fruit]['throw'] = False
 
-# Dictionary to hold the data the random fruit generation
-
+# Dictionary to hold the data for random fruit generation
 data = {}
 for fruit in fruits:
     generate_random_fruits(fruit)
@@ -59,33 +58,30 @@ def hide_cross_lives(x, y):
     gameDisplay.blit(pygame.image.load("./img/r_h.png"), (x, y))
 
 # Generic method to draw fonts on the screen
-
-font_name = pygame.font.match_font('comic.ttf')
 def draw_text(display, text, size, x, y):
-    font = pygame.font.Font(font_name, size)
+    font = pygame.font.Font(os.path.join(os.getcwd(), './fonts/DMSerifText-Regular.ttf'), size)  # Font PDBI
     text_surface = font.render(text, True, WHITE)
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
-    gameDisplay.blit(text_surface, text_rect)
+    display.blit(text_surface, text_rect)
 
-# draw players lives
-
-def draw_lives(display, x, y, lives, image) :
-    for i in range(lives) :
+# Draw player's lives
+def draw_lives(display, x, y, lives, image):
+    for i in range(lives):
         img = pygame.image.load(image)
         img_rect = img.get_rect()
         img_rect.x = int(x + 35 * i)
         img_rect.y = y
         display.blit(img, img_rect)
 
-# show game over display & front display
+# Show game over display & front display
 def show_gameover_screen():
-    gameDisplay.blit(background, (0,0))
-    draw_text(gameDisplay, "Roach Hunter", 50, WIDTH / 2, HEIGHT / 4)
-    if not game_over :
-        draw_text(gameDisplay,"Score : " + str(score), 50, WIDTH / 2, HEIGHT /2)
+    gameDisplay.blit(background, (0, 0))
+    draw_text(gameDisplay, "Roach Hunter", 95, WIDTH / 2, HEIGHT / 4)
+    if not game_over:
+        draw_text(gameDisplay, "Score : " + str(score), 50, WIDTH / 2, HEIGHT / 2)
 
-    draw_text(gameDisplay, "Press a key to begin!", 64, WIDTH / 2, HEIGHT * 3 / 4)
+    draw_text(gameDisplay, "Aperte qualquer tecla para iniciar!", 23, WIDTH / 2, HEIGHT * 3 / 4)
     pygame.display.flip()
     waiting = True
     while waiting:
@@ -100,9 +96,11 @@ def show_gameover_screen():
 first_round = True
 game_over = True
 game_running = True
-while game_running :
-    if game_over :
-        if first_round :
+paused = False  # Variable to track if the game is paused
+
+while game_running:
+    if game_over:
+        if first_round:
             show_gameover_screen()
             first_round = False
         game_over = False
@@ -111,9 +109,19 @@ while game_running :
         score = 0
 
     for event in pygame.event.get():
-        # checking for closing window
+        # Checking for closing window
         if event.type == pygame.QUIT:
             game_running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_p:  # Toggle pause
+                paused = not paused
+
+    if paused:
+        # Show pause message
+        draw_text(gameDisplay, "PAUSE", 80, WIDTH / 2, HEIGHT / 4)
+        draw_text(gameDisplay, "Aperte a 'P' para continuar", 30, WIDTH / 2, HEIGHT / 2)
+        pygame.display.update()
+        continue  # Skip game logic while paused
 
     gameDisplay.blit(background, (0, 0))
     gameDisplay.blit(score_text, (0, 0))
@@ -133,19 +141,18 @@ while game_running :
 
             current_position = pygame.mouse.get_pos()
 
-            if not value['hit'] and current_position[0] > value['x'] and current_position[0] < value['x']+60 \
-                    and current_position[1] > value['y'] and current_position[1] < value['y']+60:
+            if not value['hit'] and current_position[0] > value['x'] and current_position[0] < value['x'] + 60 \
+                    and current_position[1] > value['y'] and current_position[1] < value['y'] + 60:
                 if key == 'bomb':
                     player_lives -= 1
                     if player_lives == 0:
-                        
                         hide_cross_lives(690, 15)
-                    elif player_lives == 1 :
+                    elif player_lives == 1:
                         hide_cross_lives(725, 15)
-                    elif player_lives == 2 :
+                    elif player_lives == 2:
                         hide_cross_lives(760, 15)
 
-                    if player_lives < 0 :
+                    if player_lives < 0:
                         show_gameover_screen()
                         game_over = True
 
@@ -155,15 +162,16 @@ while game_running :
 
                 value['img'] = pygame.image.load(half_fruit_path)
                 value['speed_x'] += 10
-                if key != 'bomb' :
+                if key != 'bomb':
                     score += 1
-                score_text = font.render('Score : ' + str(score), True, (255, 255, 255))
+                score_text = font.render('Score : ' + str(score), True, WHITE)  # Update score display using PDBI font
                 value['hit'] = True
         else:
             generate_random_fruits(key)
 
     pygame.display.update()
     clock.tick(FPS)
-                        
 
 pygame.quit()
+
+
